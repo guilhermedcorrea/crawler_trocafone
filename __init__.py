7bd99348294ba85a409256086c8692527a6abdf8
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2023-03-15 22:11:12 -0300

    app
    app

---
+++ __init__.py
@@ -0,0 +1,166 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import time
+from selenium.webdriver.support import expected_conditions as EC
+import re
+from typing import Literal, List, Generator,Any
+from itertools import chain, zip_longest
+from urllib import parse
+import os
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+
+
+options = webdriver.ChromeOptions() 
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options,
+                               executable_path=r"C:\estimadoresteste\crawler_trocafone\chromedriver\chromedriver.exe")
+
+
+def scroll_page() -> None:
+    lenOfPage = driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+    while(match==False):
+        lastCount = lenOfPage
+        time.sleep(1)
+        lenOfPage = driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        if lastCount==lenOfPage:
+            match=True
+
+
+def get_sub_categorias():
+    driver.implicitly_wait(7)
+    driver.get("https://www.trocafone.com.br/")
+
+    time.sleep(1)
+
+    sub_categorias = driver.find_elements(By.XPATH
+                                          ,'/html/body/div[2]/div/div[1]/div/div[2]/div/div[2]/section/div/div[2]/div/div/nav/ul/li/div/a')
+    
+    s_categorias = [sub.get_attribute("href") for sub in sub_categorias if sub !='']
+    cont = len(s_categorias)
+    i = 0
+    while i < cont:
+        try:
+            if len(s_categorias[i]) > 3:
+                yield s_categorias[i]
+            i +=1
+        except Exception as e:
+            print(e)
+
+
+def extract_products(*args, **kwargs):
+    driver.implicitly_wait(7)
+    for arg in args:
+        driver.get(arg)
+        time.sleep(1)
+    
+def get_url_produtos():
+    driver.implicitly_wait(7)
+    categorias = get_sub_categorias()
+    try:
+        for categoria in categorias:
+            driver.get(categoria)
+            scroll_page()
+           
+            produtos_url = driver.find_elements(By.XPATH,'//*[@id="gallery-layout-container"]/div/section/a')
+            p_urls = [urls.get_attribute("href") for urls in produtos_url]
+            extract_products(*p_urls)
+            
+    except Exception as e:
+        print(e)
+
+
+lista_teste = ["https://www.trocafone.com.br/iphone-11-64-gb/p"
+               ,"https://www.trocafone.com.br/samsung-galaxy-s21-ultra-5g-256-gb/p"
+               ,"https://www.trocafone.com.br/galaxy-watch4-classic-bt-46mm-preto-bom/p"
+               "https://www.trocafone.com.br/samsung-galaxy-z-fold3-512-gb/p"
+               ,"https://www.trocafone.com.br/motorola-moto-g9-play-64-gb/p",
+               "https://www.trocafone.com.br/motorola-one-macro-64-gb/p"
+               ,"https://www.trocafone.com.br/iphone-11-pro-256-gb/p"
+               ,"https://www.trocafone.com.br/iphone-xr-256-gb/p"]
+              
+
+def produto_get():
+    lista_dict_produtos = []
+    driver.implicitly_wait(7)
+    for lista in lista_teste:
+        driver.get(lista)
+        time.sleep(1)
+        dict_produtos = {}
+
+        
+        try:
+            dict_produtos['PaginaProduto'] = lista
+            nome_produto = driver.find_elements(
+                By.XPATH,
+                '/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[1]/div/div/div[1]/div/div[2]/div/div[1]/div/div/div/h1/span')[0].text
+        
+            dict_produtos['NomeProduto'] = nome_produto
+        except Exception as e:
+            print(e)
+
+        try:
+            precos = driver.find_elements(By.XPATH,'/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[2]/div/div/div/div/div/div/div[1]/div/div[1]/div/div/div/div')
+            preco = [preco.text.replace("R$","").replace(".","").replace(",",".").strip().split("\n") for preco in precos]
+            prices = chain(preco)
+            for pric in prices:
+                dict_produtos['MenorPreco'] = min(pric)
+                dict_produtos['MaiorPreco'] = pric[0]
+        except Exception as e:
+            print(e)
+
+
+        try:
+            condicoes = driver.find_elements(By.XPATH,
+                                            '/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[1]/div/div/div[1]/div/div[2]/div/div[8]/div')
+        
+            condices_p = [condicao.text.replace("R$","").replace(".","").replace(",",".").split("\n") for condicao in condicoes]
+        
+            conds = dict(zip(*[iter(*condices_p)]*2))
+            dict_produtos.update(conds)
+        except Exception as e:
+            print(e)
+
+        try:
+            cor_produto = driver.find_elements(
+                By.XPATH,'/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[1]/div/div/div[1]/div/div[2]/div/div[4]/div/div[1]/div/div[1]/span[3]')[0].text
+        
+            dict_produtos['CorProduto'] = cor_produto
+
+            armazenamento_produto = driver.find_elements(By.XPATH,'/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[1]/div/div/div[1]/div/div[2]/div/div[5]/div[2]/button[1]')[0].text
+        
+            dict_produtos['Armazenament'] = armazenamento_produto
+        except Exception as e:
+            print(e)
+
+        try:
+            imagens = driver.find_elements(By.XPATH
+                                        ,'/html/body/div[2]/div/div[1]/div/div/div/div[3]/div/div[1]/div[2]/section/div/div[1]/div/div/div[1]/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div/div/div/div/img')
+            cont = 0
+            for imagem in imagens:
+                dict_produtos['imagem'+str(cont)] = imagem.get_attribute("srcset")
+                cont +=1
+        except Exception as e:
+            print(e)
+
+        print(dict_produtos)
+        lista_dict_produtos.append(dict_produtos)
+    
+    
+
+    return lista_dict_produtos
+
+
+listas = produto_get()
+
+data = pd.DataFrame(listas)
+data.to_excel("produtosprecos.xlsx")
+print(listas)
